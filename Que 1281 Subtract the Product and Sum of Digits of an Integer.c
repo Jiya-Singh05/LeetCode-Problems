2005a2fc509fdef41c03f23c8f9ed2fc85eaaa1c
Jiya Singh <scholarjiyasingh@gmail.com>
2026-01-10 01:44:59 +0530

    Add files via upload

---
+++ Que 1281 Subtract the Product and Sum of Digits of an Integer.c
@@ -0,0 +1,15 @@
+//Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+int subtractProductAndSum(int n) {
+    int sum=0,product=1,rem,a=n,result;
+    while(n!=0) {
+        rem=n%10;
+        sum=sum+rem;
+        n=n/10;
+    }
+        while(a!=0) {
+        rem=a%10;
+        product=product*rem;
+        a=a/10;
+    }
+    return product-sum;
+}
