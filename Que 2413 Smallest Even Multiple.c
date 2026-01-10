29ed6f1da87be28eae3cfba0b744b554a771a050
Jiya Singh <scholarjiyasingh@gmail.com>
2026-01-10 15:51:57 +0530

    Add files via upload

---
+++ Que 2413 Smallest Even Multiple.c
@@ -0,0 +1,9 @@
+//Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+int smallestEvenMultiple(int n) {
+    int a,i;
+   if(n%2==0)
+   return n;
+   else
+   a=2*n;
+   return a;
+}
