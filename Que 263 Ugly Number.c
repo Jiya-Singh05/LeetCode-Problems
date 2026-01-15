3eec33b0cc407bed5f02045b0dd7846302c2aefb
Jiya Singh <scholarjiyasingh@gmail.com>
2026-01-15 23:44:57 +0530

    Add files via upload
    Implement function to check if a number is ugly.

---
+++ Que 263 Ugly Number.c
@@ -0,0 +1,15 @@
+/*An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
+Given an integer n, return true if n is an ugly number.*/
+bool isUgly(int n) {
+    if (n<=0)
+    return 0;
+    while(n%2==0)
+    n=n/2;
+     while(n%3==0)
+    n=n/3;
+     while(n%5==0)
+    n=n/5;
+    if(n==1)
+    return 1;
+    else return 0;
+}
