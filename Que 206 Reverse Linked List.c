22c5bf8a3dac8cc759f63cdde18e7305121daee9
Jiya Singh <scholarjiyasingh@gmail.com>
2026-02-27 22:26:35 +0530

    Add files via upload

---
+++ Que 206 Reverse Linked List.c
@@ -0,0 +1,18 @@
+/*Given the head of a singly linked list, reverse the list, and return the reversed list.*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* prev=NULL,*curr=head,*next=NULL;
+    while(curr!=NULL){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    return prev;
+}
