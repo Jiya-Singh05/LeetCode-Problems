3d2f6f244b36424a43400db3bc79a6dc8550b221
Jiya Singh <scholarjiyasingh@gmail.com>
2026-03-14 07:26:23 +0530

    Add files via upload

--- Que 1051 Height Checker.c
+++ Que 1051 Height Checker.c
@@ -1,7 +1,7 @@
 /*A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height.
 Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
 You are given an integer array height representing the current order that the students are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
-Return the number of indices where heights[i] != expected[i].*/
+Return the number of indices where heights[i] !=expected[i].*/
 int heightChecker(int* heights, int heightsSize) {
     int a=heightsSize,total=0,arr[a],temp;
     for(int i=0;i<a;i++) {
