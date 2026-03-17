9382a03c5a9d29ea73c6624973d4f4de1ca4c767
Jiya Singh <scholarjiyasingh@gmail.com>
2026-03-17 15:38:15 +0530

    Add files via upload

--- Que 1137 N-th Tribonacci Number.c
+++ Que 1137 N-th Tribonacci Number.c
@@ -1,5 +1,6 @@
 /*The Tribonacci sequence Tn is defined as follows: 
-T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.Given n, return the value of Tn.*/
+T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+Given n, return the value of Tn.*/
 
 int tribonacci(int n) {
    int a=0,b=1,c=1,next;
