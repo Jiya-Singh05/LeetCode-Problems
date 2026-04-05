b893678e3db9d2f57e359fd94e705965a6571cf9
Jiya Singh <scholarjiyasingh@gmail.com>
2026-04-05 11:19:51 +0530

    Add files via upload

---
+++ Que 657 Robot Return to Origin.c
@@ -0,0 +1,26 @@
+/*There is a robot starting at the position (0, 0), the origin, on a 2D plane. Given a sequence of its moves, 
+judge if this robot ends up at (0, 0) after it completes its moves.
+
+You are given a string moves that represents the move sequence of the robot where moves[i] represents its ith move.
+Valid moves are 'R' (right), 'L' (left), 'U' (up), and 'D' (down).
+
+Return true if the robot returns to the origin after it finishes all of its moves, or false otherwise.
+
+Note: The way that the robot is "facing" is irrelevant. 'R' will always make the robot move to the right once,
+'L' will always make it move left, etc. Also, assume that the magnitude of the robot's movement is the same for each move.
+
+ 
+
+*/
+bool judgeCircle(char* moves) {
+    int d1=0,d2=0,d3=0,d4=0;
+    int total=strlen(moves);
+    for(int i=0;i<total;i++) {
+        if(moves[i]=='L') d1++;
+        if(moves[i]=='R') d2++;
+        if(moves[i]=='U') d3++;
+        if(moves[i]=='D') d4++;
+    }
+    if(d1==d2 && d3==d4) return 1;
+    return 0;
+}
