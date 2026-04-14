7af1438fb5cc9e5f0840ff5207dd33a059045b71
Jiya Singh <scholarjiyasingh@gmail.com>
2026-04-14 22:26:15 +0530

    Add files via upload

---
+++ Que 2114 Maximum Number of Words Found in Sentences.py
@@ -0,0 +1,14 @@
+# A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+
+# You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+
+# Return the maximum number of words that appear in a single sentence.
+
+ 
+class Solution:
+    def mostWordsFound(self, s: List[str]) -> int:
+        maxi=0
+        for i in range (len(s)):
+            if(s[i].count(" ")>maxi):
+                maxi=s[i].count(" ")
+        return maxi+1        
