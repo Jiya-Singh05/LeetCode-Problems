089ebfdb225c7ab25d7ff40a438e7922b4ffc559
Jiya Singh <scholarjiyasingh@gmail.com>
2026-05-17 10:23:15 +0530

    Add files via upload

---
+++ Que 557 Reverse Words in a String III.py
@@ -0,0 +1,8 @@
+#Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        l=[]
+        for i in (s.split()):
+            l.append(i[::-1])
+        return ' '.join(l)
+        
