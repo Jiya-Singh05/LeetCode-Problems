4155b85cb65fac0e71d64aabcabd8b61d45a23f1
Jiya Singh <scholarjiyasingh@gmail.com>
2026-05-22 21:17:25 +0530

    Add files via upload

--- Que 263 Ugly Number.c
+++ Que 263 Ugly Number.c
@@ -1,4 +1,5 @@
 /*An ugly number is a positive integer which does not have a prime factor other than 2, 3, and 5.
+
 Given an integer n, return true if n is an ugly number.*/
 bool isUgly(int n) {
     if (n<=0)
