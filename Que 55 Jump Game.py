c72968236ca228c0d7e2f8a76afd4b0fd2db52e1
Jiya Singh <scholarjiyasingh@gmail.com>
2026-06-20 17:52:18 +0530

    Add files via upload

---
+++ Que 55 Jump Game.py
@@ -0,0 +1,12 @@
+# You are given an integer array nums. You are initially positioned at the array's first index,
+# and each element in the array represents your maximum jump length at that position.
+
+# Return true if you can reach the last index, or false otherwise.
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        maxi=0
+        for i in range (len(nums)):
+            if i>maxi:
+                return False
+            maxi=max(maxi,i+nums[i]) 
+        return True       
