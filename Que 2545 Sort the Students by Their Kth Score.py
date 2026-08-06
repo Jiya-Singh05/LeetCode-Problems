0f2348b4659ad94712ffcaad60854c216c430357
Jiya Singh <scholarjiyasingh@gmail.com>
2026-08-06 15:28:10 +0530

    Add files via upload

---
+++ Que 2545 Sort the Students by Their Kth Score.py
@@ -0,0 +1,13 @@
+# There is a class with m students and n exams. You are given a 0-indexed m x n integer matrix score, where each row represents one student and score[i][j] denotes the score the ith student got in the jth exam. The matrix score contains distinct integers only.
+
+# You are also given an integer k. Sort the students (i.e., the rows of the matrix) by their scores in the kth (0-indexed) exam from the highest to the lowest.
+
+# Return the matrix after sorting it.
+
+class Solution:
+    def sortTheStudents(self,score:List[List[int]],k:int)->List[List[int]]:
+        for i in range(len(score)):
+            for j in range(len(score)-1):
+                if score[j][k]<score[j+1][k]:
+                    score[j],score[j+1]=score[j+1],score[j]
+        return score
