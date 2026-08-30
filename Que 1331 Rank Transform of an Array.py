90cff058d22eb706f6be7d227271632759171e01
Jiya Singh <scholarjiyasingh@gmail.com>
2026-08-30 12:51:13 +0530

    Add files via upload

---
+++ Que 1331 Rank Transform of an Array.py
@@ -0,0 +1,16 @@
+# Given an array of integers arr, replace each element with its rank.
+
+# The rank represents how large the element is. The rank has the following rules:
+
+# Rank is an integer starting from 1.
+# The larger the element, the larger the rank. If two elements are equal, their rank must be the same.
+# Rank should be as small as possible.
+
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        a=sorted(set(arr))
+        ans=[]
+        for i in arr:
+            b=bisect_left(a,i)+1 #binary search use krlo
+            ans.append(b)
+        return ans    
